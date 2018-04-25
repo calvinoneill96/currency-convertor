20d95cffa26fe6d23501fc4e4bb94c7664a887d9
calvinoneill96 <35769325+calvinoneill96@users.noreply.github.com>
2018-04-25 14:07:35 +0100

    Add in US button

--- G00353054 CA/js/convert.js
+++ G00353054 CA/js/convert.js
@@ -8,3 +8,13 @@ function euroToPounds()[
 	document.getElementById("resultsentence").innerHTML = message;
 
 ]
+function euroToUS()[
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var US = amount * 1.06;
+  var message = amount + ' euros converts to ' + US + ' US Dollars.';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+
+]
