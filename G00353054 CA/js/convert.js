9ea594925a478fefbc35afc14a4b4af2cdf8da54
calvinoneill96 <35769325+calvinoneill96@users.noreply.github.com>
2018-04-25 14:09:37 +0100

    Add in Yen button

--- G00353054 CA/js/convert.js
+++ G00353054 CA/js/convert.js
@@ -18,3 +18,13 @@ function euroToUS()[
 	document.getElementById("resultsentence").innerHTML = message;
 
 ]
+function euroToYen()[
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var Yen = amount * 121.12;
+  var message = amount + ' euros converts to ' + Yen + ' Japanese Yen.';
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+
+]
